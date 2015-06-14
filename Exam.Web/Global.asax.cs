379186607bf8acfa39c40308d89d576cf365efc4
KristianMariyanov <kristian.mariyanov@gmail.com>
2015-06-14 09:58:09 +0300

    Refactoring identity files

--- Exam.Web/Global.asax.cs
+++ Exam.Web/Global.asax.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace Exam.Web
+﻿namespace Exam.Web
 {
     using System.Reflection;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
 
     using Exam.Web.Infrastructure.Mapping;
 
